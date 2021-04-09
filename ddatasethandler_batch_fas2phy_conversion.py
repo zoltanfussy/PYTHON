ed1654456c26c4c5b7a7c76bc85b971da8e5e796
morpholino <zoltan.fussy@gmail.com>
2021-04-09 10:17:31 +0200

    script update

--- ddatasethandler_batch_fas2phy_conversion.py
+++
@@ -1,9 +0,0 @@
-from Bio import SeqIO,AlignIO
-import os
-
-files = os.listdir('.')
-files = [f.replace(".fasta","") for f in files if f.endswith(".fasta")]
-for file in files:
-	with open("{}.fasta".format(file)) as infile, open("{}.phy".format(file), "w") as outfile:
-		alignmentfile = AlignIO.read(infile, "fasta")
-		AlignIO.write(alignmentfile, outfile, "phylip-relaxed")
